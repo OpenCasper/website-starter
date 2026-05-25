fdfa5144fbd2c3a6ea577f5dde8518739e31002a
OpenCasper <OpenCasper@users.noreply.github.com>
2026-05-25 19:51:41 -0400

    Support server-side Sanity project env

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@sanity/client";
 
 export const sanityConfig = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-05-25",
+  projectId: process.env.SANITY_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: process.env.SANITY_API_VERSION || process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-05-25",
 };
 
 const readToken = process.env.SANITY_API_READ_TOKEN;
